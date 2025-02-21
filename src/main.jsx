d2e87085df0f3ad9d6da02cf1e77cb12672fd2d3
sergio-cobos <sergio.cobos@tuboleta.com>
2025-02-21 18:55:10 -0500

    feat(animated/cursor): create animated cursor component for the web page

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import '@/index.css'
 import App from '@/App.jsx'
-import Squares from './components/Squares/Squares.jsx';
+import Squares from '@/components/Squares/Squares.jsx';
+import SplashCursor from '@/components/animation/cursor';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <div className="app-wrapper">
+      <SplashCursor />
       <Squares 
         speed={0.5} 
         squareSize={40}
